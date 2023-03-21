b88a37144ea500e2e232d501063c78e3c7e77719
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-21 20:30:51 +0200

    added Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,30 @@
 const Footer = () => {
-  return <footer>Game of thrones</footer>;
+  return (
+    <footer>
+      <p>
+        Data provided by{" "}
+        <a id="thrones-api" href="https://thronesapi.com/" target="_blank">
+          thronesapi.com
+        </a>
+      </p>
+      <div className="footer-links">
+        <a
+          id="linked-in"
+          // href="UPDATE LINKED IN"
+          target="_blank"
+        >
+          <i className="fa-brands fa-linkedin-in"></i>
+        </a>
+        <a
+          id="github"
+          href="https://github.com/alleizon/memory-card"
+          target="_blank"
+        >
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
