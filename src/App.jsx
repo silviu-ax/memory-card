92fef3bfb02a55cb8f6d9fd967774185e708db0e
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-22 17:37:50 +0200

    added Score and Difficulty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-function App() {
-  const [score, setScore] = useState(0);
-
-  return (
-    <div id="app">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
-
+const App = () => (
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Score from "./Score";
+import Difficulty from "./Difficulty";
+import Game from "./Game";
 
 const Main = () => {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [difficulty, setDifficulty] = useState("easy");
+
   return (
     <main>
-      <Score />
+      <Score score={score} bestScore={bestScore} />
+      <Difficulty difficulty={difficulty} setDifficulty={setDifficulty} />
+      <Game setScore={setScore} setBestScore={setBestScore} />
     </main>
   );
 };
