5248ef979288a2c377e1920952d3a0a78ea28920
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-24 19:30:53 +0200

    refactored grid css

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -2,21 +2,27 @@ const GameCards = ({ characters, handleCardClick, images }) => {
   console.log(characters, images);
   return images.length === characters.length ? (
     characters.map((char, i) => (
-      <div key={char.id} className="grid-item">
-        <button
-          type="button"
-          onClick={() => handleCardClick(char)}
-          onKeyUp={(e) => {
-            if (e.key !== "Enter") return;
-            handleCardClick(char);
+      <div
+        key={char.id}
+        className="grid-item"
+        role="button"
+        tabIndex="0"
+        onClick={() => handleCardClick(char)}
+        onKeyUp={(e) => {
+          if (e.key !== "Enter") return;
+          handleCardClick(char);
+        }}
+      >
+        <img
+          draggable="false"
+          src={images[i].src}
+          style={{
+            objectPosition:
+              images[i].src.match(/brienne|sansa/) !== null ? "0 0" : "center",
           }}
-        >
-          <img
-            src={images[i].src}
-            width={images[i].width}
-            alt={char.fullName}
-          />
-        </button>
+          width={images[i].width}
+          alt={char.fullName}
+        />
 
         <p>{char.fullName}</p>
       </div>
